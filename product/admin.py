664aa8209b6e91b8eb1a018378d3bf5c57e7f277
Favaz <favazmohammed.vp@gmail.com>
2024-02-20 19:37:16 +0530

    productadminerror

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, ProductCategory, ProductInventory, Discount, Supplier
+from .models import Product, ProductCategory, ProductInventory, Discount, Supplier,ProductParentVariant,ProductVariantColorImages,ProductChildVariant
 # Register your models here.
 admin.site.register(Product)
 
@@ -8,3 +8,6 @@ admin.site.register(ProductCategory)
 admin.site.register(Discount)
 
 admin.site.register(Supplier)
+admin.site.register(ProductChildVariant)
+admin.site.register(ProductParentVariant)
+admin.site.register(ProductVariantColorImages)
