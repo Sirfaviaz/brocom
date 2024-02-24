3d5b140a303327f4ab6f4b4859fc1376a12d7177
Favaz <favazmohammed.vp@gmail.com>
2024-02-24 18:38:53 +0530

    cartissues

--- orders/views.py
+++ orders/views.py
@@ -375,7 +375,8 @@ def calculate_subtotal(user_id):
 
         # Accumulate total_price for subtotal
         subtotal += product.cart_set.first().quantity * discounted_price.quantize(Decimal('0.00'))
-        print(f'calcqty: {product.cart_set.first().quantity * discounted_price.quantize(Decimal('0.00'))}')
+        
+        print(f'calcqty: {product.cart_set.first().quantity} * {discounted_price}')
         print(f'calcsub: {subtotal}')
     return subtotal
 
